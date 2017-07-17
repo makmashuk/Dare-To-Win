2ff0bbcaa8b123f79f045b33e975c8cb0bdc906e
MD Mohiuddin Khan <makmashuk@gmail.com>
2017-07-17 21:40:16 +0600

    Update Dare-To-Win.cpp

--- Dare-To-Win.cpp
+++ Dare-To-Win.cpp
@@ -1,10 +1,11 @@
+
 /*
-
-*********Dare-To-Win************
-
+    *****Dare-To-Win*****
+    This is a game developed with C++ and OpenGL. 
+    In this game you have to cross the road without getting an accident where different cars are passing with different speed. 
+    Submitted By:-Khan, Md.Mohiuddin
 
 */
-
 
 
 
